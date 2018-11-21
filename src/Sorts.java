9851a5f23b1b552f1266905e063091083589880f
BT_1N3_02 <byu1510@bths.edu>
2018-11-21 10:09:58 -0500

    asd

--- src/Sorts.java
+++ src/Sorts.java
@@ -24,6 +24,48 @@ public class Sorts {
             }
         }
     }
+    public static void selectionSort(int[] arr)
+    {
+        int swap = 0;
+        int min = 0;
+        for(int i =0; i < arr.length; i++)
+        {
+            swap = i;
+            min = arr[i];
+            for(int j = i; j < arr.length; j++)
+            {
+                if(arr[j] < arr[min])
+                {
+                    swap = j;
+                    min = arr[j];
 
+                }
+            }
+        }
+        min = arr[swap];
+    }
+
+    public String[] randomStringArr(int num, int length)
+    {
+        String[]arr = new String [num];
+        while(num > 0)
+        {
+            int i =0;
+            String s = "";
+            while (i < length)
+            {
+                char c = (char)((Math.random()*26)+97);
+                s = s + c;
+                i++;
+            }
+            num--;
+            arr[num] = s;
+        }
+        return arr;
+    }
+    public void insertionSort(int[] arr)
+    {
+
+    }
 
 }
